f4351161023eacacd9a87220553b22364eaed698
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2017-12-02 11:19:52 +0000

    Updated some features

--- WineQualityPrediction.py
+++ WineQualityPrediction.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import collections
 from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
+
 # load dataset
 dataset = pd.read_csv("winequality-white.csv", delimiter=";")
 sizes = [100, 500, 1000]# 5000,10000, 50000,1000000]
@@ -14,10 +14,24 @@ Y = dataset['quality']
 X= X.astype('int')
 Y=Y.astype('int')
 models = []
+attributes = ["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides",
+    "free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"]
+for i in range(len(attributes)):
+    plt.title(attributes[i])
+    all_data = np.vstack((X.iloc[:,i], Y))
+    print(all_data)
+    #c = collections.Counter(map(tuple, all_data))
+    #print(c)
+    plt.scatter(np.asarray(X.iloc[:,i]), Y.T)
+    plt.show()
+
+
+
 models.append(('LogisticRegressionR', LogisticRegression(), 0))
 models.append(('KNN', KNeighborsClassifier(),0))
 # evaluate each model in turn
 scoring = ['homogeneity_score']
+'''
 for size in sizes:
     print("\nSize is %d" % (size))
     for name, model, score in models:
@@ -25,3 +39,4 @@ for size in sizes:
     	cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring[score])
     	mess = "%s: %f  using %s for score" % (name, cv_results.mean(),  scoring[score])
     	print(mess)
+'''
